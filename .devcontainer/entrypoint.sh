df67e90e76973563d4b2bfb214d21f7281d22fd2
Shuhao Wu <shuhao@shuhaowu.com>
2023-03-21 10:21:00 -0400

    Minor fixes

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 set -e
@@ -38,13 +37,28 @@ install_nvidia_drivers() {
       echo "Detected NVIDIA GPU, installing NVIDIA drivers..."
       local opts='--accept-license --no-questions --no-backup --ui=none --no-kernel-module --no-nouveau-check --no-kernel-module-source --no-nvidia-modprobe --install-libglvnd'
       local nvversion="$(head -n1 </proc/driver/nvidia/version | awk '{ print $8 }')"
-      wget "https://http.download.nvidia.com/XFree86/Linux-x86_64/$nvversion/NVIDIA-Linux-x86_64-$nvversion.run" -O /tmp/NVIDIA-installer.run
+      wget --progress=dot -e dotbytes=10M "https://http.download.nvidia.com/XFree86/Linux-x86_64/$nvversion/NVIDIA-Linux-x86_64-$nvversion.run" -O /tmp/NVIDIA-installer.run
       sudo sh /tmp/NVIDIA-installer.run $opts
       rm -f /tmp/NVIDIA-installer.run
     fi
   fi
 }
 
+generate_ros_domain_id_if_needed() {
+  local ros_domain_id_file=$WORKSPACE_FOLDER/.devcontainer/ros-domain-id
+  if [ ! -d $WORKSPACE_FOLDER/.devcontainer ]; then
+    echo "error: WORKSPACE_FOLDER is not mounted or WORKSPACE_FOLDER environment variable not set: $WORKSPACE_FOLDER" >&2
+    exit 1
+  fi
+
+  if [ ! -f "$ros_domain_id_file" ]; then
+    local r=$(( $RANDOM % 30 + 40 )) # ROS_DOMAIN_ID should be between 0 and 101. We choose between 40 and 70 for good luck
+    echo $r > $ros_domain_id_file
+    echo "setting ROS_DOMAIN_ID to $r"
+  fi
+}
+
 fix_dri_permissions
 install_nvidia_drivers
+generate_ros_domain_id_if_needed
 exec sleep infinity
--- .devcontainer/ros-activate.sh
+++ .devcontainer/ros-activate.sh
@@ -16,3 +16,11 @@ if [ -f $WORKSPACE_FOLDER/.devcontainer/nvidia-always-on ]; then
   export __NV_PRIME_RENDER_OFFLOAD=1
   export __GLX_VENDOR_LIBRARY_NAME=nvidia
 fi
+
+if [ -f $WORKSPACE_FOLDER/.devcontainer/ros-domain-id ]; then
+  export ROS_DOMAIN_ID=$(cat $WORKSPACE_FOLDER/.devcontainer/ros-domain-id)
+fi
+if [ -z "$ROS_DOMAIN_ID" ]; then
+  echo "WARNING: ROS_DOMAIN_ID not set. There's a chance that you may pollute others' ROS environments on this network."
+  echo "WARNING: check .devcontainer/ros-domain-id to see if it exists or if it is empty."
+fi
